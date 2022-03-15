4fec3b3016ed265fa99b7fe7da51de885b0a97e0
Jeany <jeanydevries@hotmail.com>
2022-03-15 21:14:38 +0100

    images load in list items

--- src/js/modules/renderData.js
+++ src/js/modules/renderData.js
@@ -1,18 +1,14 @@
-const main = document.querySelector('main');
+const listItems = document.getElementsByClassName("result");
 
 export function render(data) {
     const results = data.results;
     console.log(results)
     results.forEach((item, i) => {
-      const html = `
-              <article>
-                <h2>${item.titles[0]}</h2>
-                <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
-                <img src="${
-                  item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
-                }">
-              </article>
-            `;
-      main.insertAdjacentHTML('beforeend', html);
+        const html = `
+          <img src="${
+            item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
+          }">
+      `
+      listItems[i].insertAdjacentHTML('beforeend', html);
     });
   }
