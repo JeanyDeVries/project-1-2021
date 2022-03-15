d788a3ff0e6e236395c41db7bab5968de2e2ba0e
Jeany <jeanydevries@hotmail.com>
2022-03-15 21:24:01 +0100

    show image book cover in list item

--- src/js/modules/renderData.js
+++ src/js/modules/renderData.js
@@ -4,11 +4,6 @@ export function render(data) {
     const results = data.results;
     console.log(results)
     results.forEach((item, i) => {
-        const html = `
-          <img src="${
-            item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
-          }">
-      `
-      listItems[i].insertAdjacentHTML('beforeend', html);
+       listItems[i].style.backgroundImage  = `url(${item.coverimages[1]})`
     });
   }
