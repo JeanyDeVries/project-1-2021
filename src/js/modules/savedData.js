858971a0bc3257aa632e2f5cc82e7771964e7ad4
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-15 13:15:11 +0100

    added hashtags to the nuttons

---
+++ src/js/modules/savedData.js
@@ -0,0 +1,15 @@
+export let dataStartendeOnderneming;
+export let dataInnovatie;
+export let dataOndernemerschap;
+export let dataBedrijfsAnalyse;
+
+export let dataDesign;
+export let dataPrototype;
+export let dataTest;
+
+export let dataFinanciering;
+export let dataMarketingDesign;
+export let dataPlanning;
+
+export let dataJuridischeAspecten;
+export let dataManagment;
