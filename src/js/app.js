e621b9a1918aba160caec69e3f56619c7fbb0a60
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-15 14:23:31 +0100

    shows books when clicking on button, based on hash

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-const query = 'tolkien';
+let query = '';
+
 const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const detail = 'Default';
 const privateKey = "4289fec4e962a33118340c888699438d"
 const publicKey = "72a5ad5293a99ad46a9235121a3e0df2"
-const url =`${cors}${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&output=json`;
 
 import { fetchData } from "./modules/fetch.js";
 import { checkError } from "./modules/error.js";
@@ -17,4 +17,12 @@ window.addEventListener("offline", function() {
     checkError('noInternet');
 });
 
-onload = fetchData(url, config);
+window.addEventListener("hashchange", getData);
+
+function getData(){
+    query = location.hash;
+    query = `${query.substring(1)}`;
+    const url = `${cors}${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&output=json`;
+
+    fetchData(url, config);
+}
