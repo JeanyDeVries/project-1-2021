87eb1fe9a3c1613bca5a829b7b48c146452c200d
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-15 11:38:34 +0100

    changed styling road map

--- src/js/app.js
+++ src/js/app.js
@@ -3,7 +3,7 @@ const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const detail = 'Default';
 const privateKey = "4289fec4e962a33118340c888699438d"
-const publicKey = "1e19898c87464e239192c8bfe422f280"
+const publicKey = "72a5ad5293a99ad46a9235121a3e0df2"
 const url =`${cors}${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&output=json`;
 
 import { fetchData } from "./modules/fetch.js";
@@ -17,4 +17,4 @@ window.addEventListener("offline", function() {
     checkError('noInternet');
 });
 
-//onload = fetchData(url, config);
+onload = fetchData(url, config);
