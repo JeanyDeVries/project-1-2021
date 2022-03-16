ddc2a2e5a08ac3fde050d20ef7a39c3750f8bdee
Jeany de Vries <jeanydevries@hotmail.com>
2022-03-16 13:24:10 +0100

    only shows type books

--- src/js/modules/getData.js
+++ src/js/modules/getData.js
@@ -7,6 +7,8 @@ const cors = 'https://cors-anywhere.herokuapp.com/';
 const detail = 'Default';
 const privateKey = "4289fec4e962a33118340c888699438d"
 const publicKey = "72a5ad5293a99ad46a9235121a3e0df2"
+const typeBook = "facet=Type(book)"
+const refine = "refine=true"
 
 const config = {
   Authorization: `Bearer ${privateKey}`
@@ -17,6 +19,6 @@ export function getData(){
     query = `${query.substring(1)}`;
 
     //load books
-    const url = `${cors}${endpoint}${query}&authorization=${publicKey}&detaillevel=${detail}&output=json`;
+    const url = `${cors}${endpoint}${query}&${typeBook}&${refine}&authorization=${publicKey}&detaillevel=${detail}&output=json`;
     fetchData(url);
 }
